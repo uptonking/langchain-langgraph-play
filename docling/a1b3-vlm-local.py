705471ce6b43fae377a1011739d08e6adddd0ed9
uptonking <jinyaoo86@gmail.com>
2025-09-22 07:27:24 +0800

    feat: docling lmstudio vlm granite failed with looping

---
+++ docling/a1b3-vlm-local.py
@@ -0,0 +1,155 @@
+import logging
+import os
+from pathlib import Path
+
+import requests
+from dotenv import load_dotenv
+
+from docling.datamodel.base_models import InputFormat
+from docling.datamodel.pipeline_options import VlmPipelineOptions
+from docling.datamodel.pipeline_options_vlm_model import ApiVlmOptions, ResponseFormat
+from docling.document_converter import DocumentConverter, PdfFormatOption
+from docling.pipeline.vlm_pipeline import VlmPipeline
+
+# [VLM pipeline with remote model](https://docling-project.github.io/docling/examples/vlm_pipeline_api_model/)
+
+#### Using LM Studio or VLLM (OpenAI-compatible APIs)
+
+
+def openai_compatible_vlm_options(model: str, prompt: str, format: ResponseFormat):
+    options = ApiVlmOptions(
+        url="http://localhost:1234/v1/chat/completions",  # the default LM Studio
+        params=dict(
+            model=model,
+        ),
+        prompt=prompt,
+        timeout=90,
+        scale=2.0,
+        response_format=format,
+    )
+    return options
+
+
+#### Using Ollama
+
+
+def ollama_vlm_options(model: str, prompt: str):
+    options = ApiVlmOptions(
+        url="http://localhost:11434/v1/chat/completions",  # the default Ollama endpoint
+        params=dict(
+            model=model,
+        ),
+        prompt=prompt,
+        timeout=90,
+        scale=1.0,
+        response_format=ResponseFormat.MARKDOWN,
+    )
+    return options
+
+
+#### Using a cloud service like IBM watsonx.ai
+
+
+def watsonx_vlm_options(model: str, prompt: str):
+    load_dotenv()
+    api_key = os.environ.get("WX_API_KEY")
+    project_id = os.environ.get("WX_PROJECT_ID")
+
+    def _get_iam_access_token(api_key: str) -> str:
+        res = requests.post(
+            url="https://iam.cloud.ibm.com/identity/token",
+            headers={
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            data=f"grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey={api_key}",
+        )
+        res.raise_for_status()
+        api_out = res.json()
+        print(f"{api_out=}")
+        return api_out["access_token"]
+
+    options = ApiVlmOptions(
+        url="https://us-south.ml.cloud.ibm.com/ml/v1/text/chat?version=2023-05-29",
+        params=dict(
+            model_id=model,
+            project_id=project_id,
+            parameters=dict(
+                max_new_tokens=400,
+            ),
+        ),
+        headers={
+            "Authorization": "Bearer " + _get_iam_access_token(api_key=api_key),
+        },
+        prompt=prompt,
+        timeout=60,
+        response_format=ResponseFormat.MARKDOWN,
+    )
+    return options
+
+
+### Usage and conversion
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    data_folder = "/Users/yaoo/Documents/01files/testpdf"
+    input_doc_path = data_folder + "/html5canvas1page-pic.pdf"
+
+    pipeline_options = VlmPipelineOptions(
+        # <-- this is required!
+        enable_remote_services=True
+    )
+
+    # The ApiVlmOptions() allows to interface with APIs supporting
+    # the multi-modal chat interface. Here follow a few example on how to configure those.
+
+    # One possibility is self-hosting model, e.g. via LM Studio, Ollama or others.
+
+    # Example using the SmolDocling model with LM Studio:
+    # (uncomment the following lines)
+    pipeline_options.vlm_options = openai_compatible_vlm_options(
+        # model="smoldocling-256m-preview-mlx-docling-snap",
+        model="granite-docling-258m-mlx",
+        # model="granite-docling-258m",
+        prompt="Convert this page to docling.",
+        format=ResponseFormat.DOCTAGS,
+    )
+
+    # Example using the Granite Vision model with LM Studio:
+    # (uncomment the following lines)
+    # pipeline_options.vlm_options = lms_vlm_options(
+    #     model="granite-vision-3.2-2b",
+    #     prompt="OCR the full page to markdown.",
+    #     format=ResponseFormat.MARKDOWN,
+    # )
+
+    # Example using the Granite Vision model with Ollama:
+    # (uncomment the following lines)
+    # pipeline_options.vlm_options = ollama_vlm_options(
+    #     model="granite3.2-vision:2b",
+    #     prompt="OCR the full page to markdown.",
+    # )
+
+    # Another possibility is using online services, e.g. watsonx.ai.
+    # Using requires setting the env variables WX_API_KEY and WX_PROJECT_ID.
+    # (uncomment the following lines)
+    # pipeline_options.vlm_options = watsonx_vlm_options(
+    #     model="ibm/granite-vision-3-2-2b", prompt="OCR the full page to markdown."
+    # )
+
+    # Create the DocumentConverter and launch the conversion.
+    doc_converter = DocumentConverter(
+        format_options={
+            InputFormat.PDF: PdfFormatOption(
+                pipeline_options=pipeline_options,
+                pipeline_cls=VlmPipeline,
+            )
+        }
+    )
+    result = doc_converter.convert(input_doc_path)
+    print(result.document.export_to_markdown())
+
+
+if __name__ == "__main__":
+    main()
